88f2422528efda1f94b500d3bafed43d95ef7e59
SmoKkK <44506817+SmoKkK@users.noreply.github.com>
2019-02-08 04:13:10 +0200

    Update Auth.bat

--- Auth.bat
+++ Auth.bat
@@ -1,6 +1,13 @@
 @echo off
 color 0c
+echo There is update log!
+echo date:8.02.2019
+echo loader updated!
+echo +auto cache cleaner
+pause
+goto 1
 :1
+cls
 set /p pass=Login code:
 if %pass% == 87654321 start UI.bat
 if %pass% == 69696969 start UI.bat
