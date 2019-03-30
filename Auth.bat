1f463bcabe2f3a6d7f4b5e9282735e80d239896d
SmoKkK <44506817+SmoKkK@users.noreply.github.com>
2019-03-30 13:46:31 +0000

    Update Auth.bat

--- Auth.bat
+++ Auth.bat
@@ -119,6 +119,7 @@ if %pass% == 12327935 start UI.bat
 if %pass% == 17011701 start UI.bat
 if %pass% == 10082006 start UI.bat
 if %pass% == 72812384 start UI.bat
+if %pass% == 04062003 start UI.bat
 exit
 :UFF
 echo wrong/invalid code
