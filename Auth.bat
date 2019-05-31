c417145f7dcc7485ec0260978cf50c0a10b94509
SmoKkK <44506817+SmoKkK@users.noreply.github.com>
2019-05-31 15:49:58 +0300

    Update Auth.bat

--- Auth.bat
+++ Auth.bat
@@ -3,5 +3,5 @@ color 0c
 cls
 echo This Loader is Now Outdated so yeahh
 Pause >nul | echo Download the New One By pressing Any Key
-start https://cdn.discordapp.com/attachments/546028162652438528/583696481412972555/FT62.exe
+start https://discord.gg/xZZzAkt
 
