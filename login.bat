4bb16b2581de552e042e65934f80105f760bdb26
SmoKkK <44506817+SmoKkK@users.noreply.github.com>
2019-07-09 00:53:34 +0300

    Update login.bat

--- login.bat
+++ login.bat
@@ -127,6 +127,7 @@ if %pass% == 04062003 UI.bat
 if %pass% == 78447852 UI.bat
 if %pass% == 19992999 UI.bat
 if %pass% == 42042069 UI.bat
+if %pass% == 91738245 UI.bat
 :UFF
 echo wrong/invalid code
 pause
